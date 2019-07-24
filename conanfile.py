9f22cf9aec83281941d19f3fcda7fcd712259c34
Ruisheng Wang <rwang@wavecomp.com>
2019-07-24 15:09:09 -0700

    update

--- conanfile.py
+++ conanfile.py
@@ -16,8 +16,8 @@ class Antlr4Conan(ConanFile):
     def source(self):
         source_url = "https://www.antlr.org/download/antlr4-cpp-runtime-{}-source.zip".format(self.version)
         tools.get(source_url, sha256="8631a39116684638168663d295a969ad544cead3e6089605a44fea34ec01f31a", destination=self._source_subfolder)
-        antlr_file_name = "antlr-{}-complete.jar".format(self.version)
-        source_url2 = "https://www.antlr.org/download/{}".format(antlr_file_name)
+        source_url2 = "https://www.antlr.org/download/antlr-{}-complete.jar".format(self.version)
+        antlr_file_name = "antlr.jar"
         tools.download(source_url2, antlr_file_name)
         tools.check_sha256(antlr_file_name, "6852386d7975eff29171dae002cc223251510d35f291ae277948f381a7b380b4")
         shutil.move(antlr_file_name, self._source_subfolder)
@@ -35,7 +35,6 @@ class Antlr4Conan(ConanFile):
     def package(self):
         self.copy("*.h", dst="include", src=os.path.join(self._source_subfolder, "runtime", "src"))
         self.copy("*.a", dst="lib", keep_path=False)
-        shutil.move(os.path.join(self._source_subfolder, "antlr-{}-complete.jar".format(self.version)), "antlr.jar")
         self.copy("*.jar", dst="bin", keep_path=False)
 
     def package_info(self):
